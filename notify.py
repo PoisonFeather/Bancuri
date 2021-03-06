2cb90080d7564a1ab7d47ffe6254e3f3611806a3
Andrei Biro <andreibiro2003@gmail.com>
2021-03-06 23:02:18 +0200

    Update notify.py

--- notify.py
+++ notify.py
@@ -5,14 +5,16 @@ from random import randrange
 import time
 
 url = 'https://www.rotalianul.com/colectie-de-bancuri-pentru-toate-gusturile/'
-data=requests.get(url)
 
+
+data=requests.get(url)
 
 soup = BeautifulSoup(data.text,'html.parser')
 while True:
+
     banc = soup.find_all('p')[randrange(len(soup.find_all('p')))].get_text()
     print(banc)
     toaster=ToastNotifier()
-    toaster.show_toast(title="haha", msg=banc, duration=15)
+    toaster.show_toast(" ",banc,duration=10)
     print(len(banc))
     time.sleep(10)
